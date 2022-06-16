76b802aaa972e4fdfa86e821097d9b8690edfa57
sb-ebukaanene <ebuka.anene@semanticbits.com>
2022-06-16 08:03:30 -0500

    added Jenkinsfile

---
+++ scripts/build-docker-image.sh
@@ -0,0 +1,17 @@
+
+
+#!/bin/sh
+echo Logging in to Amazon ECR...
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+#export REPOSITORY_URI=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME
+
+
+echo Build started on `date`
+echo Building the Docker image...          
+docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG .
+docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG 
+
+#### push image
+echo Build completed on `date`
+echo Pushing the Docker image...
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
