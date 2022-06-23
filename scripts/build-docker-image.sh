5410621792249caa1a1776967b7eadcbfbbf131d
sb-ebukaanene <ebuka.anene@semanticbits.com>
2022-06-22 22:46:23 -0500

    added functionality for getting cloudtamer credentials

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -39,17 +39,20 @@ else
   fn_get_temporary_aws_credentials_via_aws_sts_assume_role "${ECR_REPO_ENV_AWS_ACCOUNT_NUMBER}" "${ECR_REPO_ENV}"
 fi
 
+########### Logging into ECR #################
 echo Logging in to Amazon ECR...
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 #export REPOSITORY_URI=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME
 
 
+############# Building docker image ###############
 echo Build started on `date`
 echo Building the Docker image...          
 docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG .
 docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG 
 
-#### push image
+
+##### pushing docker image to ECR ################
 echo Build completed on `date`
 echo Pushing the Docker image...
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
