6c61f49517dbd73c016c8ce26855ec18b2dc5f2c
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-07-12 14:39:31 -0700

    Update SendAndReceiveSqsEventTest.java

--- src/test/java/gov/cms/ab2d/eventlogger/utils/SendAndReceiveSqsEventTest.java
+++ src/test/java/gov/cms/ab2d/eventlogger/utils/SendAndReceiveSqsEventTest.java
@@ -36,6 +36,7 @@ public class SendAndReceiveSqsEventTest {
     @Container
     private static final AB2DLocalstackContainer LOCALSTACK_CONTAINER = new AB2DLocalstackContainer();
 
+    
     @Autowired
     private SQSEventClient sendSQSEvent;
 
