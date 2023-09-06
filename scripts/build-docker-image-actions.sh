a39213a085c41e18eed9ed993602a7678fe9c9a4
oluwolenpbc <boluwole@brians-MBP-4.attlocal.net>
2023-09-06 09:12:06 -0400

    adding build docker script for actions

---
+++ scripts/build-docker-image-actions.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -e # Turn on exit on error
+
+# Export environmental variables
+AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID
+ECR_REPO_ENV=$ECR_REPO_ENV
+DEPLOYMENT_ENV=$DEPLOYMENT_ENV
+ASSUMED_ROLE_ARN="arn:aws:iam::=$AWS_ACCOUNT_ID:role/github-actions-runner-role" 
+
+# Set default AWS region and tag
+export AWS_DEFAULT_REGION="us-east-1"
+export IMAGE_TAG="event-service"
+
+if [ "${CLOUD_TAMER}" != "false" ] && [ "${CLOUD_TAMER}" != "true" ]; then
+  echo "ERROR: CLOUD_TAMER parameter must be true or false"
+  exit 1
+elif [ "${CLOUD_TAMER}" = "false" ]; then
+
+  # Turn off verbose logging for Jenkins jobs
+  set +x
+  echo "Don't print commands and their arguments as they are executed."
+  CLOUD_TAMER="${CLOUD_TAMER}"
+
+  # Assume the specified IAM role
+  TEMP_CREDENTIALS=$(aws sts assume-role --role-arn "$ASSUMED_ROLE_ARN" --role-session-name "AssumedRoleSession")
+
+  # Set the temporary credentials as environment variables
+  export AWS_ACCESS_KEY_ID=$(echo "$TEMP_CREDENTIALS" | jq -r '.Credentials.AccessKeyId')
+  export AWS_SECRET_ACCESS_KEY=$(echo "$TEMP_CREDENTIALS" | jq -r '.Credentials.SecretAccessKey')
+  export AWS_SESSION_TOKEN=$(echo "$TEMP_CREDENTIALS" | jq -r '.Credentials.SessionToken')
+
+else # [ "${CLOUD_TAMER}" == "true" ]
+
+  # Turn on verbose logging for development machine testing
+  set -x
+  echo "Print commands and their arguments as they are executed."
+  CLOUD_TAMER="${CLOUD_TAMER}"
+
+  # Import the "get temporary AWS credentials via CloudTamer API" function
+  source "./scripts/fn_get_temporary_aws_credentials_via_cloudtamer_api.sh"
+  fn_get_temporary_aws_credentials_via_cloudtamer_api "${AWS_ACCOUNT_ID}" "${ECR_REPO_ENV}"
+
+fi
+
+########### Logging into ECR ####################
+echo Logging in to Amazon ECR...
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+#export REPOSITORY_URI=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_ENV
+
+############# Building docker image ###############
+echo Build started on `date`
+echo Building the Docker image...          
+docker build --no-cache -t $ECR_REPO_ENV:$IMAGE_TAG .
+docker tag $ECR_REPO_ENV:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_ENV:$IMAGE_TAG
+
+##### pushing docker image to ECR ################
+echo Build completed on `date`
+echo Pushing the Docker image...
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_ENV:$IMAGE_TAG
+
+####### update ECS service with new image ################
+
+aws ecs update-service --cluster $DEPLOYMENT_ENV-microservice-cluster  --service ab2d-event-service --force-new-deployment
