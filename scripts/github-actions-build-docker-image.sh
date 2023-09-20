c68d9da2486fdec9ff8ab1f412d3b9e97a839c1b
HK <hk@ip-10-232-36-9.ec2.internal>
2023-09-20 15:54:34 -0400

    using github actions credentials instead of assume in script

--- scripts/github-actions-build-docker-image.sh
+++ scripts/github-actions-build-docker-image.sh
@@ -7,14 +7,6 @@ set -e # Turn on exit on error
 export AWS_DEFAULT_REGION="us-east-1"
 export IMAGE_TAG="event-service"
 
-# assume github-runner-role
-OUT=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT_NUMBER}:role/delegatedadmin/developer/github-actions-runner-role" --role-session-name github-actions);\
-aws sts get-caller-identity
-export AWS_ACCESS_KEY_ID=$(echo $OUT | jq -r '.Credentials''.AccessKeyId');\
-export AWS_SECRET_ACCESS_KEY=$(echo $OUT | jq -r '.Credentials''.SecretAccessKey');\
-export AWS_SESSION_TOKEN=$(echo $OUT | jq -r '.Credentials''.SessionToken');
-aws sts get-caller-identity
-  
             
 
 ########### Logging into ECR ####################
@@ -36,7 +28,3 @@ docker tag $ECR_REPO_ENV:$IMAGE_TAG $ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.
 echo Build completed on `date`
 echo Pushing the Docker image...
 docker push $ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_ENV:$IMAGE_TAG
-#
-######## update ECS service with new image ################
-#
-#aws ecs update-service --cluster $DEPLOYMENT_ENV-microservice-cluster  --service ab2d-event-service --force-new-deployment
