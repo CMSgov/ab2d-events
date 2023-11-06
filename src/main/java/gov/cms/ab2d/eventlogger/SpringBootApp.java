6e7a93680eb3b0ecc3bd46c114951bad4350443c
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-11-06 10:23:15 -0500

    update springboot version to 3.1.4

--- src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
+++ src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
@@ -3,14 +3,7 @@ package gov.cms.ab2d.eventlogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-        scanBasePackages = {"gov.cms.ab2d.eventlogger", "gov.cms.ab2d.eventclient"},
-        exclude = {
-                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-                org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-                org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-        }
-)
+@SpringBootApplication(scanBasePackages = {"gov.cms.ab2d.eventlogger", "gov.cms.ab2d.eventclient"})
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
