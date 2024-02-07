27066b9d8a10d526bb8d988ede3c136255d4bdef
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-02-07 11:56:27 -0500

    bring back excludes in springbootapp

--- src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
+++ src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-        scanBasePackages = {"gov.cms.ab2d.eventlogger", "gov.cms.ab2d.eventclient"}
+        scanBasePackages = {"gov.cms.ab2d.eventlogger", "gov.cms.ab2d.eventclient"},
+        exclude = {
+            io.awspring.cloud.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+            io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration.class,
+            io.awspring.cloud.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+        }
 )
 public class SpringBootApp {
     public static void main(String[] args) {
