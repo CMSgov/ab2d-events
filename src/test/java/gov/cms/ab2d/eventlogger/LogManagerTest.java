45d1dc5befec9e6fc571d162f4717d6b8f6414ed
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-02-08 13:28:28 -0500

    remove unnecessary import

--- src/test/java/gov/cms/ab2d/eventlogger/LogManagerTest.java
+++ src/test/java/gov/cms/ab2d/eventlogger/LogManagerTest.java
@@ -27,7 +27,6 @@ import java.util.List;
 
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
