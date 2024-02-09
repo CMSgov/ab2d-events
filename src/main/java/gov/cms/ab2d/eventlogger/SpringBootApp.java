1da679b8e6aa8769858d5db3b2bc5cf5c300d4ab
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-02-08 20:49:32 -0500

    test only upgrade events client version

--- src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
+++ src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(
         scanBasePackages = {"gov.cms.ab2d.eventlogger", "gov.cms.ab2d.eventclient"},
         exclude = {
-            io.awspring.cloud.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-            io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration.class,
-            io.awspring.cloud.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+            org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+            org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+            org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+            // io.awspring.cloud.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+            // io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration.class,
+            // io.awspring.cloud.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
         }
 )
 public class SpringBootApp {
