75953fa30e09e85dd6cd2279eae8eb945b6aed54
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-02-09 15:03:32 -0500

    merge stashed changes

--- src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
+++ src/main/java/gov/cms/ab2d/eventlogger/SpringBootApp.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(
         scanBasePackages = {"gov.cms.ab2d.eventlogger", "gov.cms.ab2d.eventclient"},
         exclude = {
+            // org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+            // org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+            // org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
             io.awspring.cloud.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
             io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration.class,
-            io.awspring.cloud.autoconfigure.context.ContextRegionProviderAutoConfiguration.class,
-            io.awspring.cloud.autoconfigure.messaging.SqsAutoConfiguration.class
+            io.awspring.cloud.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
         }
 )
 public class SpringBootApp {
