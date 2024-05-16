ca74296674ee56858a5786eaa4ed001d51bcb157
oluwolenpbc <boluwole@ip-10-232-41-54.ec2.internal>
2024-05-16 13:31:57 -0400

    fix port

--- src/test/java/gov/cms/ab2d/eventlogger/utils/AB2DLocalstackContainer.java
+++ src/test/java/gov/cms/ab2d/eventlogger/utils/AB2DLocalstackContainer.java
@@ -21,7 +21,6 @@ public class AB2DLocalstackContainer extends LocalStackContainer {
         System.setProperty("com.amazonaws.sdk.disableCertChecking", "");
         super.withServices(Service.SQS);
         super.start();
-        System.setProperty("AWS_SQS_URL",
-                "localhost:" + "4566";
+        System.setProperty("AWS_SQS_URL", "localhost:4566");
     }
 }
