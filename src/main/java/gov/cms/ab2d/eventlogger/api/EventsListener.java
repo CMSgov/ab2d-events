3f50f1698b31f571ba6c4b7c7384e82c921b0fda
smirnovaae <annasmirnova@navapbc.com>
2025-01-13 13:34:02 -0800

    wip

--- src/main/java/gov/cms/ab2d/eventlogger/api/EventsListener.java
+++ src/main/java/gov/cms/ab2d/eventlogger/api/EventsListener.java
@@ -30,23 +30,27 @@ public class EventsListener {
     @SqsListener(value = "${sqs.queue-name}")//, acknowledgementMode = ON_SUCCESS)
     @Async
     public void processEvents(SQSMessages sqsMessage) {
-        log.info("EventsListener: Processing events from SQS: " + sqsMessage.getClass().getSimpleName());
-        switch (sqsMessage.getClass().getSimpleName()) {
-            case "GeneralSQSMessage" ->
-                    logManager.log(((GeneralSQSMessage) sqsMessage).getLoggableEvent());
-            case "AlertSQSMessage" ->
-                    logManager.alert(((AlertSQSMessage) sqsMessage).getMessage(), ((AlertSQSMessage) sqsMessage).getEnvironments());
-            case "TraceSQSMessage" ->
-                    logManager.trace(((TraceSQSMessage) sqsMessage).getMessage(), ((TraceSQSMessage) sqsMessage).getEnvironments());
-            case "TraceAndAlertSQSMessage" ->
-                    logManager.logAndAlert(((TraceAndAlertSQSMessage) sqsMessage).getLoggableEvent(), ((TraceAndAlertSQSMessage) sqsMessage).getEnvironments());
-            case "LogAndTraceSQSMessage" ->
-                    logManager.logAndTrace(((LogAndTraceSQSMessage) sqsMessage).getLoggableEvent(), ((LogAndTraceSQSMessage) sqsMessage).getEnvironments());
-            case "SlackSQSMessage" ->
-                    logManager.log(LogManager.LogType.SQL, ((SlackSQSMessage) sqsMessage).getLoggableEvent());
-            case "KinesisSQSMessage" ->
-                    logManager.log(LogManager.LogType.KINESIS, ((KinesisSQSMessage) sqsMessage).getLoggableEvent());
-            default -> log.info("Can't Identify Message " + sqsMessage);
+        try {
+            log.info("EventsListener: Processing events from SQS: " + sqsMessage.getClass().getSimpleName());
+            switch (sqsMessage.getClass().getSimpleName()) {
+                case "GeneralSQSMessage" -> logManager.log(((GeneralSQSMessage) sqsMessage).getLoggableEvent());
+                case "AlertSQSMessage" ->
+                        logManager.alert(((AlertSQSMessage) sqsMessage).getMessage(), ((AlertSQSMessage) sqsMessage).getEnvironments());
+                case "TraceSQSMessage" ->
+                        logManager.trace(((TraceSQSMessage) sqsMessage).getMessage(), ((TraceSQSMessage) sqsMessage).getEnvironments());
+                case "TraceAndAlertSQSMessage" ->
+                        logManager.logAndAlert(((TraceAndAlertSQSMessage) sqsMessage).getLoggableEvent(), ((TraceAndAlertSQSMessage) sqsMessage).getEnvironments());
+                case "LogAndTraceSQSMessage" ->
+                        logManager.logAndTrace(((LogAndTraceSQSMessage) sqsMessage).getLoggableEvent(), ((LogAndTraceSQSMessage) sqsMessage).getEnvironments());
+                case "SlackSQSMessage" ->
+                        logManager.log(LogManager.LogType.SQL, ((SlackSQSMessage) sqsMessage).getLoggableEvent());
+                case "KinesisSQSMessage" ->
+                        logManager.log(LogManager.LogType.KINESIS, ((KinesisSQSMessage) sqsMessage).getLoggableEvent());
+                default -> log.info("Can't Identify Message " + sqsMessage);
+            }
+        } catch (Exception e) {
+            log.error("Error processing events from SQS:" + e.getMessage());
         }
+
     }
 }
